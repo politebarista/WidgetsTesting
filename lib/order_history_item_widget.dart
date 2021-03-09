1fdcf12dc3a3c86690f92f6e3ba4ecafa715b0db
Ilya Sulimenko <twid01gc@gmail.com>
2021-03-09 20:37:11 +0700

    still working on the order history widget

--- lib/order_history_item_widget.dart
+++ lib/order_history_item_widget.dart
@@ -13,31 +13,50 @@ class OrderHistoryItemWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       child: Container(
-        width: 500,
-        height: 100,
-        color: _backgroundColor,
-        child: Column(
-          children: <Widget>[
-            Text(_date),
-            Row(
+          margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+          color: _backgroundColor,
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 5, vertical: 7),
+            child: Column(
               children: <Widget>[
-                Text('№$_orderNumber'),
-                Text(_orderStatus),
+                Row(
+                  children: <Widget>[
+                    Text(_date),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text('№$_orderNumber'),
+                    Text('-'),
+                    Text(_orderStatus),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(_orderAddress),
+                    Text('-'),
+                    Text(_orderType),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text('Заказ на сумму:'),
+                    Text('$_orderPriceР')
+                  ],
+                ),
+                GestureDetector(
+                  child: Container(
+                    color: Colors.red,
+                    child: Text('Оценить заказ'),
+                  ),
+                  onTap: () {},
+                )
               ],
             ),
-            Row(
-              children: <Widget>[
-                Text(_orderAddress),
-                Text(_orderType),
-              ],
-            ),
-            Row(children: <Widget>[
-              Text('Заказ на сумму:'),
-              Text('$_orderPriceР')
-            ],),
-          ],
-        ),
-      ),
+          )),
       onTap: () {},
     );
   }
