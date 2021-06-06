5573afe2bb0a59ad75a741d66862b120d2ce077b
Ilya Sulimenko <twid01gc@gmail.com>
2021-06-06 14:21:08 +0700

    finnaly i can convert json into object

--- lib/request_testing_widget.dart
+++ lib/request_testing_widget.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:widgets_testing/test_classes/user.dart';
 
 class RequestTestingWidget extends StatefulWidget {
   @override
@@ -22,11 +23,14 @@ class _RequestTestingWidgetState extends State<RequestTestingWidget> {
             child: Text('get data'),
             color: Colors.lightGreen,
             onPressed: () async {
-              final response = await http.get(Uri.parse('http://10.0.2.2:5000/login?login=iluha&password=krutoi')); // вместо http://127.0.0.1:5000/users, т.к у андоридов другой локалхост
+              final response = await http.get(Uri.parse(
+                  'http://10.0.2.2:5000/login?login=iluha&password=krutoi')); // вместо http://127.0.0.1:5000/users, т.к у андоридов другой локалхост
 
               final decode = json.decode(response.body) as Map<String, dynamic>;
 
-             print('check');
+              var some = User.fromJson(decode);
+              var asdasd = response.body;
+              print(asdasd.runtimeType);
               userName = decode['login'];
               setState(() {});
             },
